91f06c91631a7433c729203acf4002ae39262c1d
Rômulo Ramos <rmsramos@gmail.com>
2024-06-23 22:34:25 -0300

    new translation files added

---
+++ resources/lang/es/modal.php
@@ -0,0 +1,8 @@
+<?php
+
+// translations for Rmsramos/ChangeUser
+return [
+    'modal_heading' => 'Cambiar de usuario',
+    'email'         => 'Correo electrónico',
+    'password'      => 'Contraseña',
+];
